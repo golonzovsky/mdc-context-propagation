7fbd24b2630cd50e5f2f0efad38d9bfebef05ae1
golonzovsky <alex.golonzovsky@gmail.com>
2015-10-18 18:58:28 +0200

    cleanup

--- src/main/java/org/golonzovsky/Application.java
+++ src/main/java/org/golonzovsky/Application.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ImportResource("itineraryPricingFlowGatewayContext.xml")
+@ImportResource("asyncFlowIntegrationContext.xml")
 public class Application {
 
   public static void main(String[] args) {
